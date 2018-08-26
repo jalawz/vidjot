60dcdf08281d20a3eb56738407449835b519e2e2
paulomenezes-sciensa <paulo.roberto@sciensa.com>
2018-08-26 18:18:51 -0300

    Flash messages and session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -29,6 +31,24 @@ app.use(bodyParser.json());
 
 // Method Override middleware
 app.use(methodOverride('_method'));
+
+// Express Session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Flash middleware
+app.use(flash());
+
+// Global variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 // Index Route
 app.get('/', (req, res) => {
@@ -94,6 +114,7 @@ app.post('/ideas', (req, res) => {
         new Idea(newUser)
             .save()
             .then(idea => {
+                req.flash('success_msg', 'Video idea added');
                 res.redirect('/ideas');
             });
     }
@@ -110,6 +131,7 @@ app.put('/ideas/:id', (req, res) => {
 
         idea.save()
             .then(idea => {
+                req.flash('success_msg', 'Video idea updated');
                 res.redirect('/ideas');
             });
     });
@@ -119,6 +141,7 @@ app.put('/ideas/:id', (req, res) => {
 app.delete('/ideas/:id', (req, res) => {
     Idea.remove({ _id: req.params.id })
         .then(() => {
+            req.flash('success_msg', 'Video idea removed');
             res.redirect('/ideas');
         });
 });
